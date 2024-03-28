934833c983567f9d3406e9313605a0018b593845
Raj Kashikar <65059626+raj200501@users.noreply.github.com>
2024-03-28 15:59:01 -0400

    Create tests.c

---
+++ tests.c
@@ -0,0 +1,97 @@
+/* Testing Code */
+
+#include <limits.h>
+#include <math.h>
+
+/* Routines used by floation point test code */
+
+/* Convert from bit level representation to floating point number */
+float u2f(unsigned u) {
+  union {
+    unsigned u;
+    float f;
+  } a;
+  a.u = u;
+  return a.f;
+}
+
+/* Convert from floating point number to bit-level representation */
+unsigned f2u(float f) {
+  union {
+    unsigned u;
+    float f;
+  } a;
+  a.f = f;
+  return a.u;
+}
+
+/* Copyright (C) 1991-2021 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
+/* This header is separate from features.h so that the compiler can
+   include it implicitly at the start of every compilation.  It must
+   not itself include <features.h> or any other header that includes
+   <features.h> because the implicit include comes before any feature
+   test macros that may be defined in a source file before it first
+   explicitly includes a system header.  GCC knows the name of this
+   header in order to preinclude it.  */
+/* glibc's intent is to support the IEC 559 math functionality, real
+   and complex.  If the GCC (4.9 and later) predefined macros
+   specifying compiler intent are available, use them to determine
+   whether the overall intent is to support these features; otherwise,
+   presume an older compiler has intent to support these features and
+   define these macros by default.  */
+/* wchar_t uses Unicode 10.0.0.  Version 10.0 of the Unicode Standard is
+   synchronized with ISO/IEC 10646:2017, fifth edition, plus
+   the following additions from Amendment 1 to the fifth edition:
+   - 56 emoji characters
+   - 285 hentaigana
+   - 3 additional Zanabazar Square characters */
+//1
+int test_bitXor(int x, int y)
+{
+  return x^y;
+}
+//2
+int test_fitsBits(int x, int n)
+{
+  int TMin_n = -(1 << (n-1));
+  // This convoluted way of generating TMax avoids overflow
+  int TMax_n = (int) ((1u << (n-1)) - 1u);
+  return x >= TMin_n && x <= TMax_n;
+}
+//3
+int test_rotateRight(int x, int n) {
+  unsigned u = (unsigned) x;
+  int i;
+  for (i = 0; i < n; i++) {
+      unsigned lsb = (u & 1) << 31;
+      unsigned rest = u >> 1;
+      u = lsb | rest;
+  }
+  return (int) u;
+}
+//4
+int test_bitReverse(int x) {
+    int result = 0;
+    int i;
+    for (i = 0; i < 32; i++) {
+ int bit = (x >> i) & 0x1;
+ int pos = 31-i;
+ result |= bit << pos;
+    }
+    return result;
+}
